edc6e8e571eb325958941840175fef5fd5ae2e20
Eder Soares <edersoares@me.com>
2022-07-12 21:26:19 -0300

    Menus

---
+++ database/sqls/inserts/menus.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.menus VALUES (6, NULL, 'Transporte escolar', NULL, '/intranet/educar_transporte_escolar_index.php', 'fa-bus', 7, 1, 69, 69, NULL, true, NULL, NULL);
+INSERT INTO public.menus VALUES (44, 27, 'Empresas', 'Empresas do transporte', '/intranet/transporte_empresa_lst.php', NULL, 0, 3, 21235, 21235, 20710, true, NULL, NULL);
+INSERT INTO public.menus VALUES (45, 27, 'Motoristas', 'Motoristas do transporte', '/intranet/transporte_motorista_lst.php', NULL, 0, 3, 21236, 21236, 20710, true, NULL, NULL);
+INSERT INTO public.menus VALUES (46, 27, 'Pontos', 'Rotas do transporte', '/intranet/transporte_ponto_lst.php', NULL, 0, 3, 21238, 21238, 20710, true, NULL, NULL);
+INSERT INTO public.menus VALUES (47, 27, 'Rotas', 'Pontos do transporte', '/intranet/transporte_rota_lst.php', NULL, 0, 3, 21239, 21239, 20710, true, NULL, NULL);
+INSERT INTO public.menus VALUES (48, 27, 'Veículos', 'Veículos do transporte', '/intranet/transporte_veiculo_lst.php', NULL, 0, 3, 21237, 21237, 20710, true, NULL, NULL);
+INSERT INTO public.menus VALUES (50, 28, 'Usuários de transporte', 'Usuários de transporte', '/intranet/transporte_pessoa_lst.php', NULL, 0, 3, 21240, 21240, 20711, true, NULL, NULL);
+INSERT INTO public.menus VALUES (100, 49, 'Cópia de rotas', 'Cópia de rotas do transporte', '/intranet/transporte_copia_rotas.php', NULL, 0, 4, 21246, 21246, 21244, true, NULL, NULL);
