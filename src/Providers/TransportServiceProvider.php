1fb9cc80faaef6e69c755e947929594e88f5c4d1
Eder Soares <edersoares@me.com>
2022-07-14 11:29:43 -0300

    Mapeia as rotas do módulo

--- src/Providers/TransportServiceProvider.php
+++ src/Providers/TransportServiceProvider.php
@@ -33,14 +33,43 @@ class TransportServiceProvider extends ServiceProvider
     public static function module(): array
     {
         return [
-            //
+            'Api/Views/EmpresaController',
+            'Api/Views/MotoristaController',
+            'Api/Views/PessoatransporteController',
+            'Api/Views/PontoController',
+            'Api/Views/RotaController',
+            'Api/Views/VeiculoController',
+            'TransporteEscolar/Views/EmpresaController',
+            'TransporteEscolar/Views/ItinerarioController',
+            'TransporteEscolar/Views/MotoristaController',
+            'TransporteEscolar/Views/PessoatransporteController',
+            'TransporteEscolar/Views/PontoController',
+            'TransporteEscolar/Views/RotaController',
+            'TransporteEscolar/Views/VeiculoController',
         ];
     }
 
     public static function intranet(): array
     {
         return [
-            //
+            'intranet/educar_transporte_escolar_index.php',
+            'intranet/ponto_xml.php',
+            'intranet/transporte_copia_rotas.php',
+            'intranet/transporte_empresa_det.php',
+            'intranet/transporte_empresa_lst.php',
+            'intranet/transporte_index.php',
+            'intranet/transporte_itinerario_cad.php',
+            'intranet/transporte_itinerario_del.php',
+            'intranet/transporte_motorista_det.php',
+            'intranet/transporte_motorista_lst.php',
+            'intranet/transporte_pessoa_det.php',
+            'intranet/transporte_pessoa_lst.php',
+            'intranet/transporte_ponto_det.php',
+            'intranet/transporte_ponto_lst.php',
+            'intranet/transporte_rota_det.php',
+            'intranet/transporte_rota_lst.php',
+            'intranet/transporte_veiculo_det.php',
+            'intranet/transporte_veiculo_lst.php',
         ];
     }
 }
