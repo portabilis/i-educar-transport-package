4855bcf6b7d5e076c1edb7fce1156f92ebf73459
Eder Soares <edersoares@me.com>
2022-07-14 11:08:38 -0300

    Utiliza dados diretos na migration

--- database/migrations/2020_01_01_190022_default_data_modules_tipo_veiculo.php
+++ database/migrations/2020_01_01_190022_default_data_modules_tipo_veiculo.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 
 class DefaultDataModulesTipoVeiculo extends Migration
@@ -13,7 +12,17 @@ class DefaultDataModulesTipoVeiculo extends Migration
      */
     public function up()
     {
-        Artisan::call('db:seed', ['--class' => DefaultModulesTipoVeiculoTableSeeder::class, '--force' => true]);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 1, 'descricao' => 'Vans/Kombis']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 2, 'descricao' => 'Microônibus']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 3, 'descricao' => 'Ônibus']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 4, 'descricao' => 'Bicicleta']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 5, 'descricao' => 'Tração Animal']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 6, 'descricao' => 'Outro']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 7, 'descricao' => 'Capacidade de até 5 Alunos']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 8, 'descricao' => 'Capacidade entre 5 a 15 Alunos']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 9, 'descricao' => 'Capacidade entre 15 a 35 Alunos']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 10, 'descricao' => 'Capacidade acima de 35 Alunos']);
+        DB::table('modules.tipo_veiculo')->insert(['cod_tipo_veiculo' => 11, 'descricao' => 'Trem/Metrô']);
     }
 
     /**
